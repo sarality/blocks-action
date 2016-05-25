4db2ec0fd1299be276fd7eaac08168d149c253fc
Abhideep Singh <abhi@dothat.in>
2016-05-25 11:17:05 +0530

    Implementation of OnClick Trigger and action to change activity

---
+++ src/main/java/com/dothat/action/StartActivityAction.java
@@ -0,0 +1,35 @@
+package com.dothat.action;
+
+import android.app.Activity;
+import android.content.Intent;
+
+/**
+ * Action to show a View
+ *
+ * @author abhideep@ (Abhideep Singh)
+ */
+public class StartActivityAction implements ViewAction {
+
+  private final Activity activity;
+  private final Class<? extends Activity> activityClass;
+  private boolean showAnimation;
+
+  public StartActivityAction(Activity activity, Class<? extends Activity> activityClass, boolean showAnimation) {
+    this.activity = activity;
+    this.activityClass = activityClass;
+    this.showAnimation = showAnimation;
+  }
+
+  public StartActivityAction(Activity activity, Class<? extends Activity> activityClass) {
+    this(activity, activityClass, false);
+  }
+
+  @Override
+  public void perform() {
+    Intent intent = new Intent(activity, activityClass);
+    if (!showAnimation) {
+      intent.addFlags(Intent.FLAG_ACTIVITY_NO_ANIMATION);
+    }
+    activity.startActivity(intent);
+  }
+}
