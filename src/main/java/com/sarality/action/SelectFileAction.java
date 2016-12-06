cc6451c32715d4dd01cce376300325783e2ed7b8
Abhideep Singh <abhi@dothat.in>
2016-12-06 21:31:11 +0530

    Action that opens a File Chooser

---
+++ src/main/java/com/sarality/action/SelectFileAction.java
@@ -0,0 +1,33 @@
+package com.sarality.action;
+
+import android.app.Activity;
+import android.content.Intent;
+
+/**
+ * Starts a File Chooser Dialog.
+ *
+ * @author abhideep@ (Abhideep Singh)
+ */
+public class SelectFileAction implements ViewAction {
+
+  private final Activity activity;
+  private final String fileType;
+  private final int titleResourceId;
+  private final int resultCode;
+
+  public SelectFileAction(Activity activity, String fileType, int titleResourceId, int resultCode) {
+    this.activity = activity;
+    this.fileType = fileType;
+    this.titleResourceId = titleResourceId;
+    this.resultCode = resultCode;
+  }
+
+  @Override
+  public boolean perform(ActionContext actionContext) {
+    Intent chooseFile = new Intent(Intent.ACTION_GET_CONTENT);
+    chooseFile.setType(fileType);
+    chooseFile = Intent.createChooser(chooseFile, activity.getResources().getString(titleResourceId));
+    activity.startActivityForResult(chooseFile, resultCode);
+    return true;
+  }
+}
